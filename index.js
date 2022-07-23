5ed9031407db96e9803b5dfe765710aef698937e
2022_i_1_Vashche <vital10@inbox.ru>
2022-07-23 23:05:48 +0300

    Some work.

--- index.js
+++ index.js
@@ -49,3 +49,17 @@ alert(array.length);
 array.forEach(function(item, index, array) {
   return console.log(item)
 });
+
+//Задание 8
+let family = new Map([
+  ["mother", "salad"],
+  ["father", "beer"],
+  ["son",    "meat"]
+]);
+for (let name of family.keys()) {
+  console.log('ключ-' + name);
+};
+for (let food of family.values()) {
+  console.log('Значение-' + food);
+};
+//Есть понимание, что это задание выполнено неверно/не до конца, что необходимо вывести в консоль 3 позиции, а не 6, как в моем случае. Нахожусь на пути решения этой задачи
